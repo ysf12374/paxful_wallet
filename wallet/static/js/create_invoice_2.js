c75c0a3a6b94843e643407a7fdf970dd19227e79
Yousuf <yousuf.mohammad@student.unisi.it>
2020-12-07 21:23:48 +0100

    Removed Unnecessary .js files

--- wallet/static/js/create_invoice_2.js
+++
@@ -1,8 +0,0 @@
-$('#printInvoice').click(function(){
-        Popup($('.invoice')[0].outerHTML);
-        function Popup(data) 
-        {
-            window.print();
-            return true;
-        }
-    });
