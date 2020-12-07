beef6ef5605e3459c23bf1677631cf865ad04c11
Yousuf <yousuf.mohammad@student.unisi.it>
2020-12-07 21:34:31 +0100

    commneted crete_session

--- pages/urls.py
+++ pages/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('api/v1/wallet/api', views.api, name='api'),
     path('api/v1/wallet/details_long', views.details_long, name='details_long'),
     path('api/v1/wallet/details_short', views.details_short, name='details_short'),
-    path('api/v1/wallet/create_session', views.create_session, name='create_session'),
-    path('api/v1/wallet/create_address', views.create_address, name='create_address'),
+    # path('api/v1/wallet/create_session', views.create_session, name='create_session'),
+    # path('api/v1/wallet/create_address', views.create_address, name='create_address'),
     path('api/v1/wallet/transfer_funds/<int:amount>', views.transfer_funds, name='transfer'),
     path('api/v1/wallet/generate', views.generate, name='generate'),
 ]
