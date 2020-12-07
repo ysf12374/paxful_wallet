a9bd32d9ff4cf3d9fe18d1ed3975bab2dda63d58
ysf12374 <47970186+ysf12374@users.noreply.github.com>
2020-12-07 05:23:13 +0100

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-# from .models import CompanyData,CompanyAnnReports,CompanyAnalysis,InvoiceHash,InvoiceMetadata,InvoicePdfMetadata,LicenseTable,StatusOfUpdate,UploadInvoice
 
 # admin.site.register(CompanyData)
